b36675f096fdcced043318f9271f2a6af2467d4a
s.magdevich <Stanislav.Magdevich.consultant@nielseniq.com>
2023-06-14 20:40:00 +0300

    dz_8

---
+++ src/main/java/DZ_8/Lake.java
@@ -0,0 +1,13 @@
+package DZ_8;
+
+public class Lake {
+    String name;
+
+    public Lake(String name) {
+        this.name = name;
+    }
+
+    public String printLake() {
+        return name;
+    }
+}
