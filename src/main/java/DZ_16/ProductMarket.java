7cd87946aea628057ea7efde400e811e7b3b847e
s.magdevich <Stanislav.Magdevich.consultant@nielseniq.com>
2023-07-21 12:13:31 +0300

    dz_16_add_main_class

--- src/main/java/DZ_16/ProductMarket.java
+++ src/main/java/DZ_16/ProductMarket.java
@@ -57,22 +57,4 @@ public class ProductMarket {
         return String.join(" ", prices);
     }
 
-
-    public static void main(String[] args) {
-        List<Product> productList = new ArrayList<>();
-        productList.add(new Product("молоко", 2.99));
-        productList.add(new Product("хлеб", 0.99));
-        productList.add(new Product("творог", 6.99));
-        productList.add(new Product("сметана", 4.99));
-        productList.add(new Product("яблоко", 0.99));
-        productList.add(new Product("вино", 10.99));
-        ProductMarket obj = new ProductMarket(productList);
-        System.out.println(obj.getAllProductName());
-        System.out.println(obj.getAllProductSortedByName());
-        System.out.println(obj.getAllProductByHigherPrice());
-        System.out.println(obj.getAllProductByLowerPrice());
-        System.out.println(obj.getAllPricesAsString());
-    }
-
-
 }
