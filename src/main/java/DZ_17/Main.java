0fe3997aafec5436aae0d374901bc491c502fe73
s.magdevich <Stanislav.Magdevich.consultant@nielseniq.com>
2023-07-25 11:11:06 +0300

    dz_17

---
+++ src/main/java/DZ_17/Main.java
@@ -0,0 +1,20 @@
+package DZ_17;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("молоко", 2.99));
+        productList.add(new Product("хлеб", 0.99));
+        productList.add(new Product("творог", 6.99));
+        productList.add(new Product("сметана", 4.99));
+        productList.add(new Product("яблоко", 0.99));
+        productList.add(new Product("вино", 10.99));
+        ProductMarket obj = new ProductMarket(productList);
+        System.out.println(obj.sortProductByPrice());
+    }
+}
